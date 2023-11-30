157f58b3c3c7fa651280d89cb2ad6cc7c89b03d6
Yash Kulkarni <110209883+yaasshhh20@users.noreply.github.com>
2023-11-30 18:32:48 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,12 @@
+function redirectToSite() {
+    window.location.href = 'https://github.com/yaasshhh20';
+}
+function gotosite() {
+    window.location.href='https://leetcode.com/yaasshhh20/';
+}
+$(document).ready(function(){
+    $('.flip').click(function(){
+      $('.cont-flip').toggleClass('flipped');
+      return false;
+    });
+  });
